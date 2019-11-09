e94e455a2fecae4e3988a63021f1b9d80602a47d
andreykhorkov <and.khorkov@gmail.com>
2019-11-09 12:31:23 -0800

    initial

---
+++ Assets/Scripts/ShipInputHandler.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using Zenject;
+
+public class ShipInputHandler : ITickable
+{
+    readonly Transform _transform;
+    private float _speed;
+
+    public ShipInputHandler(
+        float speed,
+        Transform transform)
+    {
+        _transform = transform;
+        _speed = speed;
+    }
+
+    public void SetSpeed(float speed)
+    {
+        _speed = speed;
+    }
+
+    public void Tick()
+    {
+        Debug.Log(_speed);
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            _transform.position += Vector3.forward * _speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            _transform.position -= Vector3.forward * _speed * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Scripts/ShipInstaller.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Zenject;
+
+public class ShipInstaller : Installer<ShipInstaller>
+{
+    readonly float _speed;
+
+    public ShipInstaller(
+        [InjectOptional]
+        float speed)
+    {
+        _speed = speed;
+        Debug.Log(_speed);
+    }
+
+    public override void InstallBindings()
+    {
+        Container.Bind<ShipFacade>().AsSingle();
+        Container.Bind<Transform>().FromComponentOnRoot();
+        Container.BindInterfacesAndSelfTo<ShipInputHandler>().AsSingle();
+        Container.BindInstance(_speed).WhenInjectedInto<ShipInputHandler>();
+        Container.Bind<ShipHealthHandler>().FromNewComponentOnRoot().AsSingle();
+    }
+}
